85a290ccf8e523176cd0cae511d41f43a884e76d
Abdulrahman <abd3lra7man.078@gmail.com>
2025-07-04 17:56:38 +0300

    Create Value Objects.

---
+++ RealEstate.Domain/ValueObjects/Amenities.cs
@@ -0,0 +1,42 @@
+﻿namespace RealEstate.Domain.ValueObjects;
+
+public class Amenities
+{
+    public bool HasAirConditioning { get; private set; }
+    public bool HasHeating { get; private set; }      
+    public bool IsFurnished { get; private set; }
+    public bool HasSwimmingPool { get; private set; }
+    public bool HasFireplace { get; private set; }
+    public bool HasGarden { get; private set; }
+    public bool HasSecuritySystem { get; private set; }
+    public bool? HasSmokingArea { get; private set; }
+    public bool HasParking { get; private set; }
+
+    private Amenities() { }
+
+    public Amenities(bool hasAirConditioning, bool hasHeating, bool isFurnished, bool hasSwimmingPool, bool hasFireplace, bool hasGarden, bool hasSecuritySystem, bool? hasSmokingArea, bool hasParking)
+    {
+        HasAirConditioning = hasAirConditioning;
+        HasHeating = hasHeating;
+        IsFurnished = isFurnished;
+        HasSwimmingPool = hasSwimmingPool;
+        HasFireplace = hasFireplace;
+        HasGarden = hasGarden;
+        HasSecuritySystem = hasSecuritySystem;
+        HasSmokingArea = hasSmokingArea;
+        HasParking = hasParking;
+    }
+
+    // protected override IEnumerable<object> GetEqualityComponents()
+    // {
+    //     yield return HasAirConditioning;
+    //     yield return HasHeating;
+    //     yield return IsFurnished;
+    //     yield return HasSwimmingPool;
+    //     yield return HasFireplace;
+    //     yield return HasGarden;
+    //     yield return HasSecuritySystem;
+    //     yield return HasSmokingArea ?? false;
+    //     yield return HasParking;
+    // }
+}
---
+++ RealEstate.Domain/ValueObjects/Facilities.cs
@@ -0,0 +1,33 @@
+﻿namespace RealEstate.Domain.ValueObjects;
+
+public class Facilities
+{
+    public int Area { get; private set; }      
+    public int Rooms { get; private set; }
+    public int Kitchens { get; private set; }
+    public int Balconies { get; private set; } 
+    public int Baths { get; private set; }
+    public int Beds { get; private set; }
+
+    private Facilities() { }
+
+    public Facilities(int area, int rooms, int kitchens, int balconies, int baths, int beds)
+    {
+        Area = area;
+        Rooms = rooms;
+        Kitchens = kitchens;
+        Balconies = balconies;
+        Baths = baths;
+        Beds = beds;
+    }
+
+    // protected override IEnumerable<object> GetEqualityComponents()
+    // {
+    //     yield return Area;
+    //     yield return Rooms;
+    //     yield return Kitchens;
+    //     yield return Balconies;
+    //     yield return Baths;
+    //     yield return Beds;
+    // }
+}
