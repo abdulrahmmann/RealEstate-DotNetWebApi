bf2e3b32507c99727957477dc19614d640002711
Abdulrahman <abd3lra7man.078@gmail.com>
2025-07-04 17:13:33 +0300

    Create BaseEntity Abstract Class.

---
+++ RealEstate.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    
+    public DateTime CreatedOn { get; set; }
+    
+    public DateTime? ModifiedOn { get; set; }
+}
